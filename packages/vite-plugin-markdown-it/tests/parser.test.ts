9702303eed5a087486710468eb6c4c0c960bda19
lixu <lx01588155@alibaba-inc.com>
2024-12-16 14:08:46 +0800

    feat: 优化打包

--- packages/vite-plugin-markdown-it/tests/parser.test.ts
+++ packages/vite-plugin-markdown-it/tests/parser.test.ts
@@ -3,9 +3,9 @@ import { fileURLToPath } from 'node:url';
 import { normalizePath } from 'vite';
 
 import { describe, it } from 'vitest';
-import { Parser } from '../src/core/parser';
 // @ts-expect-error this is raw
 import test from '../../example/src/base/alert.md?raw';
+import { Parser } from '../src/core/parser';
 
 const srcDir = normalizePath(fileURLToPath(new URL('../../example/src/base', import.meta.url)));
 
@@ -21,10 +21,10 @@ describe('parser', async () => {
     const id = 'alert.md';
     await parser.load(id, {
       fence: false,
-      fileName: `../example/src/base/${id}`,
+      filePath: `../example/src/base/${id}`,
       componentIndex: 0,
     });
-    const source = await parser.transform(test, id, { fileName: 'alert.md' } as any);
+    const source = await parser.transform(test, id, { filePath: 'alert.md' } as any);
 
     console.log(source);
   });
