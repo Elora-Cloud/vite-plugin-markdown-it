d555005838074c4064062fd4249446bb9872eb4d
lixu <lx01588155@alibaba-inc.com>
2025-04-21 15:13:18 +0800

    chore: 删除重复样式文件

--- packages/example/src/main.ts
+++ packages/example/src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import '@elora-cloud/vite-plugin-markdown-it/dist/components/es/components/style/index';
-import '@elora-cloud/vite-plugin-markdown-it/dist/components/es/style.css';
+import '@elora-cloud/vite-plugin-markdown-it/dist/components/dist/vite-plugin-markdown-it.css';
 
 import 'element-plus/theme-chalk/src/var.scss';
 
